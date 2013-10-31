c2be7c6d37e347669007c2dc2bfe2cbbc4557eaf
Matt Schmulen <matt.schmulen@gmail.com>
2013-10-31 09:47:47 -0700

    convert to cli

--- app.js
+++ app.js
@@ -1,35 +1,16 @@
-/**
- * Main application
- */
+var math = require('./math');
 
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , path = require('path');
+var parameter = 0;
 
-var app = express();
+var args = process.argv.splice(2);
+//console.log(args);
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
-});
+if (typeof args[0] !== 'undefined' && args[0] !== null) {
+	parameter = args[0];
+}
+	
+var returnValue = math.fib( parameter );
+console.log(returnValue);
+//debug('fib result:'+returnValue);
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
-
-var options = {};
-
-routes(app, options);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("fib listening on port " + app.get('port'));
-});
-
+return returnValue;
